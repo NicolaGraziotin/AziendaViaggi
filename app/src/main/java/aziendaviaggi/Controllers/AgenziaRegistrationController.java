efec0aaf0eee26bb6eeb402d4f0d7ab2ef6633c2
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-05-20 23:23:37 +0200

    Add registration controllers for agenzia and cliente

---
+++ app/src/main/java/aziendaviaggi/Controllers/AgenziaRegistrationController.java
@@ -0,0 +1,33 @@
+package aziendaviaggi.Controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class AgenziaRegistrationController {
+    
+    @FXML
+    private Pane AgenziaReg;
+
+    @FXML
+    private void back(ActionEvent event) {
+        changeScene(event, "/login.fxml");
+    }
+
+    private void changeScene(ActionEvent event, String url) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(url));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
