fa03a2533148d05b915b5a18965a582e81d8189a
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-05-20 22:35:23 +0200

    Add clientRegistration fxml

--- app/src/main/java/aziendaviaggi/Controllers/LoginController.java
+++ app/src/main/java/aziendaviaggi/Controllers/LoginController.java
@@ -2,7 +2,13 @@ package aziendaviaggi.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 public class LoginController {
@@ -30,5 +36,13 @@ public class LoginController {
 
     @FXML
     private void handleRegCliente(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/clientRegistration.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
