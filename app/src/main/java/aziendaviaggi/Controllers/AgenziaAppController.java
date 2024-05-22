3e8b18fb3db090d49cc83ebf8e24d85251ac3d2d
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-05-22 12:42:57 +0200

    Create table view in agenzia app instead of list view

--- app/src/main/java/aziendaviaggi/Controllers/AgenziaAppController.java
+++ app/src/main/java/aziendaviaggi/Controllers/AgenziaAppController.java
@@ -3,25 +3,52 @@ package aziendaviaggi.Controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import aziendaviaggi.Objects.Pacchetto;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
 public class AgenziaAppController extends ControllerFactory {
 
     @FXML
     private Pane AgenziaAppl;
+    
+    @FXML
+    private TableView<Pacchetto> TableV;
 
     @FXML
-    private ListView<String> ListV;
+    private TableColumn<Pacchetto, String> ColumnDesc;
+
+    @FXML
+    private TableColumn<Pacchetto, String> ColumnNome;
+
+    @FXML
+    private TableColumn<Pacchetto, String> ColumnPrez;
+
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<String> items = FXCollections.observableArrayList("Ciao", "Dio", "Cane");
-        ListV.setItems(items);
+        ColumnNome.setCellValueFactory(new PropertyValueFactory<Pacchetto,String>("nome"));
+        ColumnDesc.setCellValueFactory(new PropertyValueFactory<Pacchetto,String>("descrizione"));
+        ColumnPrez.setCellValueFactory(new PropertyValueFactory<Pacchetto,String>("prezzo"));
+
+        ObservableList<Pacchetto> list = FXCollections.observableArrayList(
+            new Pacchetto("Hawaii", "Isola con spiaggia privata", "1000"),
+            new Pacchetto("Hawaii", "Isola con spiaggia privata", "1000"),
+            new Pacchetto("Hawaii", "Isola con spiaggia privata", "1000"),
+            new Pacchetto("Hawaii", "Isola con spiaggia privata", "1000"),
+            new Pacchetto("Hawaii", "Isola con spiaggia privata", "1000")
+        );
+
+        TableV.setItems(list);
     }
 
     @FXML
@@ -31,12 +58,15 @@ public class AgenziaAppController extends ControllerFactory {
 
     @FXML
     private void delete(ActionEvent event) {
-        ObservableList<String> items = ListV.getItems();
-        if (items.isEmpty()) {
+        ObservableList<Pacchetto> selected, list;
+        try {
+            list = TableV.getItems();
+            selected = TableV.getSelectionModel().getSelectedItems();
+            for (Pacchetto pacchetto : selected) {
+                list.remove(pacchetto);
+            }
+        } catch (Exception e) {
             alertThrower("Non ci sono piu' pacchetti!");
-        } else {
-            String deleting = ListV.getSelectionModel().getSelectedItem();
-            items.remove(deleting);
         }
     }
 
